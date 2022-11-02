580f471a849d013ce5d310788be9919e4c5182a9
Gunnar Hilling <gunnar@hilling.de>
2022-11-02 09:10:24 +0100

    working quarkus, including tests.

--- route-files/solutions/customer-support-requests/src/test/java/com/redhat/training/processingorders/FtpToFileRouteBuilderTest.java
+++ route-files/solutions/customer-support-requests/src/test/java/com/redhat/training/processingorders/FtpToFileRouteBuilderTest.java
@@ -12,52 +12,49 @@ import org.apache.camel.EndpointInject;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.AdviceWith;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.language.bean.Bean;
 import org.apache.camel.quarkus.test.CamelQuarkusTestSupport;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 class FtpToFileRouteBuilderTest extends CamelQuarkusTestSupport {
 
-	@Produce("direct:ftp")
-	protected ProducerTemplate template;
+    @Produce("direct:ftp")
+    protected ProducerTemplate template;
 
-	@Inject
-	protected CamelContext context;
+    @Inject
+    protected CamelContext context;
 
-	@EndpointInject( "mock:file:customer_requests" )
-	protected MockEndpoint fileMock;
+    @EndpointInject("mock:file:customer_requests")
+    protected MockEndpoint fileMock;
 
-	@Configuration
-	public static class TestConfig {
-		@Produces
-		RoutesBuilder route() {
-			return new FtpToFileRouteBuilder();
-		}
-	}
-	@Override
-	protected void doBeforeEach(QuarkusTestMethodContext context) throws Exception {
-		mockRouteEndpoints();
-	}
+    @Configuration
+    public static class TestConfig {
+        @Produces
+        RoutesBuilder route() {
+            return new FtpToFileRouteBuilder();
+        }
+    }
 
-	@Test
-	void testFTPFileContentIsWrittenToFile() throws Exception {
-		fileMock.message( 0 ).body().isEqualTo( "Hello World" );
-		template.sendBody( "direct:ftp", "Hello World" );
-		fileMock.assertIsSatisfied();
-	}
+    @Override
+    protected void doBeforeEach(QuarkusTestMethodContext context) throws Exception {
+        AdviceWith.adviceWith(this.context, "ftpRoute", FtpToFileRouteBuilderTest::enhanceRoute);
+    }
 
-	private void mockRouteEndpoints() throws Exception {
-		AdviceWithRouteBuilder.adviceWith(context, "ftpRoute", route ->
-			route.replaceFromWith("direct:ftp"));
-		AdviceWithRouteBuilder.adviceWith(context, "ftpRoute", route ->
-			route.interceptSendToEndpoint( "file:.*customer_requests.*" )
-				 .skipSendToOriginalEndpoint()
-				 .to( "mock:file:customer_requests" ));
-	}
+    @Test
+    void testFTPFileContentIsWrittenToFile() throws Exception {
+        fileMock.message(0).body().isEqualTo("Hello World");
+        template.sendBody("direct:ftp", "Hello World");
+        fileMock.assertIsSatisfied();
+    }
+
+    private static void enhanceRoute(AdviceWithRouteBuilder route) {
+        route.replaceFromWith("direct:ftp");
+        route.interceptSendToEndpoint("file:.*customer_requests.*")
+             .skipSendToOriginalEndpoint()
+             .to("mock:file:customer_requests");
+    }
 
 }
