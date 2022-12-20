633e988640bae436a0da4ae38f864c1731310fbb
Gunnar Hilling <gunnar@hilling.de>
2022-12-20 16:04:32 +0100

    fix customer-support-requests/apps

--- route-files/apps/customer-support-requests/src/main/java/com/redhat/training/processingorders/FtpToFileRouteBuilder.java
+++ route-files/apps/customer-support-requests/src/main/java/com/redhat/training/processingorders/FtpToFileRouteBuilder.java
@@ -1,12 +1,10 @@
 package com.redhat.training.processingorders;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.stereotype.Component;
 
 // TODO: Enable the route by extending the RouteBuilder superclass
-@Component
 public class FtpToFileRouteBuilder {
 
     // TODO: Implement the configure method
-    @Override
+    //@Override
 }
--- route-files/apps/customer-support-requests/src/test/java/com/redhat/training/processingorders/FtpToFileRouteBuilderTest.java
+++ route-files/apps/customer-support-requests/src/test/java/com/redhat/training/processingorders/FtpToFileRouteBuilderTest.java
@@ -1,67 +1,60 @@
 package com.redhat.training.processingorders;
 
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.callback.QuarkusTestMethodContext;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+
 import org.apache.camel.CamelContext;
+import org.apache.camel.Configuration;
 import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.AdviceWith;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.test.spring.CamelSpringBootRunner;
-import org.apache.camel.test.spring.UseAdviceWith;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.apache.camel.quarkus.test.CamelQuarkusTestSupport;
+import org.junit.jupiter.api.Test;
 
-@RunWith( CamelSpringBootRunner.class )
-@SpringBootTest
-@UseAdviceWith
-public class FtpToFileRouteBuilderTest {
+@QuarkusTest
+class FtpToFileRouteBuilderTest extends CamelQuarkusTestSupport {
 
-	@Autowired
-	private ProducerTemplate template;
+	@Produce("direct:ftp")
+	protected ProducerTemplate template;
 
-	@Autowired
-	private CamelContext context;
+	@Inject
+	protected CamelContext context;
 
-	@EndpointInject( uri = "mock:file:customer_requests" )
-	MockEndpoint fileMock;
+	@EndpointInject("mock:file:customer_requests")
+	protected MockEndpoint fileMock;
 
-	@Before
-	public void setUp() throws Exception {
-		mockRouteEndpoints();
-		context.start();
+	@Configuration
+	public static class TestConfig {
+		@Produces
+		RoutesBuilder route() {
+			return new FtpToFileRouteBuilder();
+		}
 	}
 
-	@After
-	public void tearDown() throws Exception {
-		context.stop();
+	@Override
+	protected void doBeforeEach(QuarkusTestMethodContext context) throws Exception {
+		AdviceWith.adviceWith(this.context, "ftpRoute", FtpToFileRouteBuilderTest::enhanceRoute);
 	}
 
 	@Test
-	public void testFTPFileContentIsWrittenToFile() throws Exception {
-		fileMock.message( 0 ).body().isEqualTo( "Hello World" );
-
-		template.sendBody( "direct:ftp", "Hello World" );
-
+	void testFTPFileContentIsWrittenToFile() throws Exception {
+		fileMock.message(0).body().isEqualTo("Hello World");
+		template.sendBody("direct:ftp", "Hello World");
 		fileMock.assertIsSatisfied();
 	}
 
-	private void mockRouteEndpoints() throws Exception {
-		context.getRouteDefinition( "ftpRoute" )
-				.adviceWith( context, new AdviceWithRouteBuilder() {
-					@Override
-					public void configure() {
-						// Do not use the real FTP endpoint
-						replaceFromWith( "direct:ftp" );
-
-						// Do not write to a real file
-						interceptSendToEndpoint( "file:.*customer_requests.*" )
-								.skipSendToOriginalEndpoint()
-								.to( "mock:file:customer_requests" );
-					}
-				} );
+	private static void enhanceRoute(AdviceWithRouteBuilder route) {
+		route.replaceFromWith("direct:ftp");
+		route.interceptSendToEndpoint("file:.*customer_requests.*")
+			 .skipSendToOriginalEndpoint()
+			 .to("mock:file:customer_requests");
 	}
 
 }
