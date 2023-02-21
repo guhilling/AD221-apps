eb634465fdda31c3363b2a93e5c01376dc4e45c5
Gunnar Hilling <gunnar@hilling.de>
2023-02-21 16:39:11 +0100

    ch5

--- async-jms/apps/async-jms/src/main/java/com/redhat/training/messaging/AMQPRouteBuilder.java
+++ async-jms/apps/async-jms/src/main/java/com/redhat/training/messaging/AMQPRouteBuilder.java
@@ -1,18 +1,17 @@
 package com.redhat.training.messaging;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.stereotype.Component;
 
-
-@Component
 public class AMQPRouteBuilder extends RouteBuilder {
 
-	public static String ROUTE_NAME = "amqp-order-input";
+	public static final String ROUTE_NAME = "amqp-order-input";
 
 	@Override
-	public void configure() throws Exception {
-
-		// TODO: receive messages from AMQP Queue and send  to the log-orders route
-
+	public void configure() {
+		// TODO: receive messages from AMQP queue and send  to the log-orders route
+		from("amqp:queue:amqp_order_input")
+			.routeId(ROUTE_NAME)
+			.log("AMQPRouteBuilder: Processing Non-delivered Orders")
+			.to("direct:log_orders");
 	}
 }
--- async-jms/apps/async-jms/src/test/java/com/redhat/training/messaging/JmsRouteBuilderTest.java
+++ async-jms/apps/async-jms/src/test/java/com/redhat/training/messaging/JmsRouteBuilderTest.java
@@ -1,97 +1,72 @@
 package com.redhat.training.messaging;
 
-import static com.redhat.training.messaging.Application.OrderProducer;
-import com.redhat.training.model.Order;
+import io.quarkus.test.junit.QuarkusTest;
 
-import org.apache.camel.ProducerTemplate;
-import org.apache.camel.ConsumerTemplate;
 import org.apache.camel.EndpointInject;
-import org.apache.camel.CamelContext;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.AdviceWith;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.test.spring.CamelSpringBootRunner;
+import org.apache.camel.quarkus.test.CamelQuarkusTestSupport;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
-import org.junit.Before;
-import org.junit.runner.RunWith;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.math.BigDecimal;
-import java.lang.Integer;
+import com.redhat.training.model.Order;
 
 
-@RunWith(CamelSpringBootRunner.class)
-@SpringBootTest(classes = {Application.class},
-    properties = { "camel.springboot.java-routes-include-pattern=**/JmsRoute*"})
-public class JmsRouteBuilderTest {
-
+@QuarkusTest
+class JmsRouteBuilderTest extends CamelQuarkusTestSupport {
 
     private static final String MOCK_RESULT_LOG = "mock:result_log";
     private static final String MOCK_RESULT_AMQP = "mock:result_amqp";
 
-    @EndpointInject(uri = MOCK_RESULT_LOG)
-    private MockEndpoint resultLogEndpoint;
+    @Produce("amqp:queue:jms_order_input")
+    protected ProducerTemplate producerTemplate;
 
-    @EndpointInject(uri = MOCK_RESULT_AMQP)
-    private MockEndpoint resultAMQPEndpoint;
+    @EndpointInject(MOCK_RESULT_LOG)
+    protected MockEndpoint resultLogEndpoint;
 
-    @Autowired
-    private CamelContext camelContext;
+    @EndpointInject(MOCK_RESULT_AMQP)
+    protected MockEndpoint resultAMQPEndpoint;
 
-    @Autowired
-    private ProducerTemplate producerTemplate;
-
-    @Before
-    public void setup() throws Exception {
-	    
-	camelContext.getRouteDefinition(JmsRouteBuilder.ROUTE_NAME)
-		.autoStartup(true)
-                .adviceWith(camelContext, new AdviceWithRouteBuilder() {
-                    @Override
-                    public void configure() throws Exception {
-                        interceptSendToEndpoint("direct:log_orders")
-                                .skipSendToOriginalEndpoint()
-                                .to(MOCK_RESULT_LOG);
-                        interceptSendToEndpoint("amqp:queue:amqp_order_input")
-                                .skipSendToOriginalEndpoint()
-                                .to(MOCK_RESULT_AMQP);
-                    }
-                });
+    @Override
+    protected RoutesBuilder createRouteBuilder() {
+        return new JmsRouteBuilder();
     }
 
     @Test
-    public void testJmsOrderRoute() throws Exception {
+    void testJmsOrderRoute() throws Exception {
+        resultLogEndpoint.expectedMessageCount(1);
+        resultAMQPEndpoint.expectedMessageCount(1);
 
-	resultLogEndpoint.expectedMessageCount(1);
-    resultAMQPEndpoint.expectedMessageCount(1);
+        OrderProducer orderProducer  = new OrderProducer();
 
-        // Builds sample test data
-        OrderProducer orderProducer  = new OrderProducer(
-		new Integer(5),
-		new BigDecimal("0.012"),
-		false,
-		"Test Order 1"
-        );
-
-        OrderProducer orderProducer2  = new OrderProducer(
-		new Integer(10),
-		new BigDecimal("0.015"),
-		true,
-		"Test Order 2"
-        );
-
-        Order testOrder = orderProducer.getOrder();
-        Order testOrder2 = orderProducer2.getOrder();
+        Order testOrder = orderProducer.createOrder();
+        Order testOrder2 = orderProducer.createOrder();
 
         // Sends messages to the start component
-        producerTemplate.sendBody("jms:queue:jms_order_input", testOrder);
-        producerTemplate.sendBody("jms:queue:jms_order_input", testOrder2);
+        producerTemplate.sendBody(testOrder);
+        producerTemplate.sendBody(testOrder2);
 
         // Verifies that a message received
 	    resultLogEndpoint.assertIsSatisfied();
         resultAMQPEndpoint.assertIsSatisfied();
     }
 
+    @BeforeEach
+    void doAdvice() throws Exception {
+        AdviceWith.adviceWith(context(), JmsRouteBuilder.ROUTE_NAME,
+                              JmsRouteBuilderTest::adviceRoute);
+    }
+
+    private static void adviceRoute(AdviceWithRouteBuilder route) {
+        route.interceptSendToEndpoint("direct:log_orders")
+            .skipSendToOriginalEndpoint()
+            .to(MOCK_RESULT_LOG);
+        route.interceptSendToEndpoint("amqp:queue:amqp_order_input")
+            .skipSendToOriginalEndpoint()
+            .to(MOCK_RESULT_AMQP);
+    }
 }
