f1b62c2ebd73628a5430537e72282273f9f83d69
Maurizio Montel <dr.prodigy.github@gmail.com>
2023-02-23 17:06:33 +0100

    ch03s02 TODO

--- pattern-filter/apps/pattern-filter/src/main/java/com/redhat/training/transform/OrderLogRouteBuilder.java
+++ pattern-filter/apps/pattern-filter/src/main/java/com/redhat/training/transform/OrderLogRouteBuilder.java
@@ -6,9 +6,11 @@ public class OrderLogRouteBuilder extends RouteBuilder {
 
     public void configure() throws Exception {
         // TODO: add direct route to mock order log end point
+        /*
         from("direct:jsonOrderLog")
         .routeId("Log Orders")
         .log("Order received: ${body}")
         .to("mock:orderLog");
+        */
     }
 }
