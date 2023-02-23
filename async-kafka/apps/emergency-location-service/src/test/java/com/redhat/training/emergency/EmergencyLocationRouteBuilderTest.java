556da7d72e284bcf1c5e24255effbe44cb544098
Gunnar Hilling <gunnar@hilling.de>
2023-02-23 10:56:25 +0100

    kafka

--- async-kafka/apps/emergency-location-service/src/test/java/com/redhat/training/emergency/EmergencyLocationRouteBuilderTest.java
+++ async-kafka/apps/emergency-location-service/src/test/java/com/redhat/training/emergency/EmergencyLocationRouteBuilderTest.java
@@ -14,14 +14,12 @@ import org.apache.camel.builder.AdviceWith;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.quarkus.test.CamelQuarkusTestSupport;
-import org.apache.camel.quarkus.test.support.kafka.KafkaTestResource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.redhat.training.emergency.route.EmergencyLocationRouteBuilder;
 
 @QuarkusTest
-@QuarkusTestResource(KafkaTestResource.class)
 class EmergencyLocationRouteBuilderTest extends CamelQuarkusTestSupport {
 
 	@Inject
@@ -30,23 +28,18 @@ class EmergencyLocationRouteBuilderTest extends CamelQuarkusTestSupport {
 	@EndpointInject("mock:file:logger")
 	protected MockEndpoint loggerEndpoint;
 
-	//@Override
-	//protected RoutesBuilder createRouteBuilder() {
-	//	return new EmergencyLocationRouteBuilder();
-	//}
+	@Override
+	protected RoutesBuilder createRouteBuilder() {
+		return new EmergencyLocationRouteBuilder();
+	}
 
 	@Test
-	void emptyTest() throws Exception {
-		loggerEndpoint.expectedMessageCount(1);
+	void testEmergencyLocationRoute() throws Exception {
+		loggerEndpoint.expectedMessageCount(2);
 		loggerEndpoint.assertIsSatisfied(1000);
 	}
 
-	@Test
-	void testEmergencyLocationRoute() {
-		assertErrorNotOccured();
-	}
-
-	//@BeforeEach
+	@BeforeEach
 	void doAdvice() throws Exception {
 		AdviceWith.adviceWith(context(), "emergency-location-route",
 							  EmergencyLocationRouteBuilderTest::adviceRoute);
@@ -58,8 +51,4 @@ class EmergencyLocationRouteBuilderTest extends CamelQuarkusTestSupport {
 			.to("mock:file:logger");
 	}
 
-	private void assertErrorNotOccured() {
-		String body = consumerTemplate.receive("direct:output").getIn().getBody(String.class);
-		assertNotEquals("errorOccured", body); 
-	}
 }
